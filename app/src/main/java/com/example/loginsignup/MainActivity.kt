2e3320c8e880ee293c0d3ae286989d7b35a55afd
Hannah <hama.lafuente.up@phinmaed.com>
2024-01-25 11:21:44 +0800

    first change

---
+++ app/src/main/java/com/example/loginsignup/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.loginsignup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+    //Change 1
